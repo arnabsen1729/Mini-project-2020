87624f6e67d9b06d7bae7c779d3f5251e196456e
Mr-DareDevil <arnabsen1729@gmail.com>
2021-03-03 02:15:07 +0530

    Algorithm added: arnabsen1729

--- Algorithms/xmlToDataStructure/convert.js
+++ Algorithms/xmlToDataStructure/convert.js
@@ -1,4 +1,10 @@
-// const fs = require('fs');
+/*
+ *
+ * XML To Data Structure
+ * =====================
+ * Converting the XML doc to the required data structure (object)
+ *
+ */
 
 function xmlToJson2(xml) {
     var obj = {};
@@ -76,13 +82,13 @@ function parseXml(xml) {
     return dom;
 }
 
+// returns the entire dataStructure
 function parseRawJSON(data) {
-    console.log(data);
     transitions = data['DATA']['state-diagram']['transition'];
-    finalData = {};
+    let finalData = {};
 
     finalData['transition'] = [];
-    states = new Set();
+    let states = new Set();
     transitions.forEach((trans) => {
         finalData['transition'].push({
             present_state: trans['present_state']['#text'],
@@ -94,15 +100,84 @@ function parseRawJSON(data) {
         states.add(trans['present_state']['#text']);
         states.add(trans['next_state']['#text']);
     });
-    finalData['state-count'] = states.size;
+    finalData['state_count'] = states.size;
     finalData['states'] = [...states];
-
-    console.log(states);
+    let maxBits = parseInt(Math.log2(finalData['state_count'] - 1)) + 1;
+    finalData['gates_count'] = maxBits;
     return finalData;
 }
 
-sampleXML = `<DATA><transitions><states><data>A</data><data>B</data><data>C</data><data>D</data></states><inputs><data>0</data><data>1</data></inputs><outputs><data>0</data><data>1</data></outputs><next_states><data>A</data><data>B</data><data>C</data><data>D</data></next_states></transitions><state-diagram><transition><present_state>00</present_state><input>1</input><output>1</output><next_state>00</next_state></transition><transition><present_state>00</present_state><input>0</input><output>0</output><next_state>01</next_state></transition><transition><present_state>01</present_state><input>1</input><output>0</output><next_state>11</next_state></transition><transition><present_state>01</present_state><input>0</input><output>1</output><next_state>10</next_state></transition><transition><present_state>11</present_state><input>0</input><output>0</output><next_state>11</next_state></transition><transition><present_state>11</present_state><input>1</input><output>1</output><next_state>10</next_state></transition><transition><present_state>10</present_state><input>1</input><output>0</output><next_state>01</next_state></transition><transition><present_state>10</present_state><input>0</input><output>1</output><next_state>00</next_state></transition></state-diagram></DATA>`;
-console.log('XML DOC\n', parseXml(sampleXML));
-console.log('JSON Data Structure\n');
+/*
+ *
+ * Algorithm for solving
+ * =====================
+ * 1. State Assignment
+ * 2. Solving Circuit Excitation Table
+ *
+ */
+
+// state Assignment
+function stateAssign(data) {
+    let maxBits = data['gates_count'];
+    let maxValue = Math.pow(2, maxBits);
+    let states = [];
+    for (i = 0; i < maxValue; i += 1) {
+        states.push(i.toString(2).padStart(maxBits, '0'));
+    }
+    let presentStates = data['states'];
+    presentStates.sort();
+    let stateMap = {};
+    presentStates.forEach((present, index) => {
+        stateMap[present] = states[index];
+    });
+    console.log(stateMap);
+
+    return stateMap;
+}
+
+function updateStates(data, stateMap) {
+    let newData = { ...data };
+    newData['transition'] = data['transition'].map((trans) => {
+        return {
+            ...trans,
+            present_state: stateMap[trans.present_state],
+            next_state: stateMap[trans.next_state],
+        };
+    });
+
+    console.log(newData);
+    return newData;
+}
+//
+
+function getDecimalValue(output, present_state) {
+    return parseInt(output + present_state, 2);
+}
+
+function getDFFValues(data) {
+    let DFFValues = [];
+    for (let gateIndex = 0; gateIndex < data['gates_count']; gateIndex += 1) {
+        let gateValues = [];
+        data.transition.forEach((trans) => {
+            if (trans.next_state.charAt(gateIndex) === '1') {
+                console.log(trans);
+                console.log(getDecimalValue(trans.output, trans.present_state));
+                gateValues.push(
+                    getDecimalValue(trans.output, trans.present_state)
+                );
+            }
+        });
+        DFFValues.push(gateValues);
+    }
+
+    console.log(DFFValues);
+}
+
+sampleXML = `<DATA><transitions><states><data>A</data><data>B</data><data>C</data><data>D</data></states><inputs><data>0</data><data>1</data></inputs><outputs><data>0</data><data>1</data></outputs><next_states><data>A</data><data>B</data><data>C</data><data>D</data></next_states></transitions><state-diagram><transition><present_state>A</present_state><input>1</input><output>1</output><next_state>A</next_state></transition><transition><present_state>A</present_state><input>0</input><output>0</output><next_state>B</next_state></transition><transition><present_state>B</present_state><input>1</input><output>0</output><next_state>D</next_state></transition><transition><present_state>B</present_state><input>0</input><output>1</output><next_state>C</next_state></transition><transition><present_state>D</present_state><input>0</input><output>0</output><next_state>D</next_state></transition><transition><present_state>D</present_state><input>1</input><output>1</output><next_state>C</next_state></transition><transition><present_state>C</present_state><input>1</input><output>0</output><next_state>B</next_state></transition><transition><present_state>C</present_state><input>0</input><output>1</output><next_state>A</next_state></transition></state-diagram></DATA>`;
 jsonData = xmlToJson(parseXml(sampleXML));
-console.log(parseRawJSON(jsonData));
+let finalData = parseRawJSON(jsonData);
+console.log(finalData);
+let stateMap = stateAssign(finalData);
+finalData = updateStates(finalData, stateMap);
+finalData['gate'] = 'D';
+getDFFValues(finalData);
